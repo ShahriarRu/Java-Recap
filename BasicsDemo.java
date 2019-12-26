9e049763905bc2bde285176881f2f677392458db
ShahriarRu <shahriarcse26@gmail.com>
2019-12-27 00:12:29 +0600

    Initial Commit with Gitignore and basic demo

---
+++ BasicsDemo.java
@@ -0,0 +1,12 @@
+ class BasicsDemo{
+
+ 	static void print(){
+ 		System.out.println("Hello World");
+ 		System.out.println();
+ 		System.out.print("World");
+ 	}
+
+	public static void main(String[] args) {
+ 		print();
+ 	}
+ }
